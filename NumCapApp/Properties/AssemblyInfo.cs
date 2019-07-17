4ce1fd330318fe632e9bd59642f63a1a52bf41e0
Mr Mystery <kick.punch.productions@gmail.com>
2019-07-17 19:57:52 +1000

    Update copyright to creative commons.
    Not fit for any purpose. Use at own risk etc.

--- NumCapApp/Properties/AssemblyInfo.cs
+++ NumCapApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NumCapApp")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Creative Commons 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
